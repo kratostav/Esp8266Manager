612d5f4e1de8af9b6c8a7efb6fadf7f02a5fb87d
aschrett <andre.schretter@edu.htl-villach.at>
2016-01-16 18:19:20 +0100

    IDE Helper Debugbar

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
          * Third party
          */
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
@@ -212,6 +214,7 @@ return [
          */
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
